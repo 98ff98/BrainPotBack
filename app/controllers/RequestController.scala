698ff7c594bb69f8f927060e2cccc86aa57e67d8
LEE JUNSEOK <scala-in@naver.com>
2017-06-20 20:08:04 +0900

    자동 팀삭제 구현

--- app/controllers/RequestController.scala
+++ app/controllers/RequestController.scala
@@ -1,10 +1,27 @@
 package controllers
 
+import java.util.concurrent.TimeUnit
+
+import akka.actor.ActorSystem
+import com.google.inject.Inject
+import models.{AutoTeamRemover, TeamManager}
 import play.api._
+import play.api.db.DBApi
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-class RequestController extends Controller {
+import scala.concurrent.duration.Duration
+
+class RequestController @Inject()(actorSystem: ActorSystem , dBApi: DBApi) extends Controller {
+  { // 10분마다 오래된 팀들을 자동으로 삭제한다.
+    val scheduler = actorSystem.scheduler
+    val runnableTask = new AutoTeamRemover(new TeamManager(dBApi))
+    implicit val executor = actorSystem.dispatcher
+    scheduler.schedule(
+      initialDelay = Duration(300, TimeUnit.SECONDS),
+      interval = Duration(300, TimeUnit.SECONDS),
+      runnable = runnableTask)
+  }
 
   def index = Action {
     Ok(views.html.index())
--- app/models/TeamManager.scala
+++ app/models/TeamManager.scala
@@ -1,6 +1,21 @@
 package models
 
+import javax.inject.Inject
 
-class TeamManager {
+import anorm.SQL
+import play.api.db.DBApi
+
+
+class TeamManager @Inject() (dbApi: DBApi) {
+  private val db = dbApi.database("default")
+
+  def callCLEAR_OLD_TEAMS() : Unit = {
+    db.withConnection { implicit collection =>
+      SQL("CALL `CLEAR_OLD_TEAMS`()").executeUpdate()
+    }
+  }
+
+}
+object TeamManager{
 
 }
