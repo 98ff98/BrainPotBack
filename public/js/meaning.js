814e5955cb6a2e6a23eedf7b3ad8678c100c3359
SpiralMoon <mjh1660@gmail.com>
2017-08-15 03:05:52 +0900

    request 94 ver grouping to meaning 데이터 변환

--- public/js/meaning.js
+++ public/js/meaning.js
@@ -9,6 +9,32 @@ var Meaning = {
             $("#menu2").css("display", "none");
             $(".canvas-container").css("display", "none");
 
+            //<code>load data grouping to meaning</code>
+            var div = $("#brain_field_3")[0];
+
+            data.forEach(function (item) {
+                var chips = "";
+                item.node.forEach(function (node) {
+                    chips += '<div class="chip green">' + node + '</div>';
+                });
+
+                div.innerHTML += '<div class="row center">' +
+                    '<div class="col s2"></div>' +
+                    '<div class="col s8 z-depth-1">' +
+                    '<div>' +
+                    '<h4 class="left">' + item.title + '</h4>' +
+                    chips +
+                    '<div class="input-field col s6">' +
+                    '<input id="test" type="text" class="validate">' +
+                    '<label for="test">comment<label>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '<div class="col s2"></div>' +
+                    '</div>';
+            });
+            //<code>load data grouping to meaning</code>
+
             
         }
     }
